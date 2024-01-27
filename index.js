bfe6d81cabddb08a8c3eb629c715e3f657868555
Vincent Revard <Vincent.Revard@gmail.com>
2024-01-27 00:08:44 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+
+const currentUser = 'Vinny';
+
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// onsole.log(excitedWelcomeMessage);
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
